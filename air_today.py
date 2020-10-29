bc5307b2471b3e17255d6adc4837c9bce26491d6
Jbaik <joungwoo.baik@gmail.com>
2020-10-29 02:20:29 -0700

    Finish air today info

---
+++ air_today.py
@@ -0,0 +1,17 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+session = HTMLSession()
+
+url = "https://www.airnow.gov/?city=San%20Francisco&state=CA&country=USA"
+
+
+r = session.get(url)
+
+r.html.render(sleep=1, keep_page=True, scrolldown=1)
+
+aqis = r.html.find('.pollutant-info-heading')
+
+for aqi in aqis:
+    print(aqi.text, end="\n \n \n")
